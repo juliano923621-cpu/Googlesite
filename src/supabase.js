f8339f7c862e308364951b6a4a60de2ff1b9f241
juliano <juliano923621@gmail.com>
2026-04-28 22:11:27 -0400

    Corrige chave do Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://gktiihufvqupyovdukzg.supabase.co';
-const supabaseKey = 'sb_publishable_Xw6clTISTBh6RoAqhznlrQ_8oBKs_XA';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrdGlpaHVmdnF1cHlvdmR1a3pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0MjAyNjIsImV4cCI6MjA5Mjk5NjI2Mn0.PzRyrUDyPsD8-vq_oEfosSl-BlEtLM0ixwUVmEWS_bY';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
